27b7b39978bd00a2d8512de6141e5918707d444b
zahra <zahra.h.gholipour@gmail.com>
2023-12-16 02:13:35 +0330

    added orsey

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,1 @@
 const carousel = new bootstrap.Carousel("#myCarousel");
-const myCarousel = document.getElementById("myCarousel");
-
-myCarousel.addEventListener("slide.bs.carousel", (event) => {
-  // do something...
-});
